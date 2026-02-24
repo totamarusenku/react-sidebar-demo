47efdc1eff669e33e4954ec8c22081583049e2ff
The Daijin <6650280+totamarusenku@users.noreply.github.com>
2026-02-24 14:25:44 +0530

    Create new project default screen

---
+++ src/components/NoProjectSelected.jsx
@@ -0,0 +1,24 @@
+import Button from "./Button";
+import noProjectImage from "../assets/no-projects.png";
+const NoProjectSelected = () => {
+  return (
+    <div className="mt-24 text-center w-2/3">
+      <img
+        src={noProjectImage}
+        alt="No Project Image"
+        className="w-16 h-16 object-contain mx-auto"
+      />
+      <h2 clasName="text-xl font-bold text-stone-500 my-4">
+        No Project Selected
+      </h2>
+      <p className="text-stone-400 mb-4">
+        Select a project or create a new one
+      </p>
+      <p className="mt-8">
+        <Button>Create Project</Button>
+      </p>
+    </div>
+  );
+};
+
+export default NoProjectSelected;
